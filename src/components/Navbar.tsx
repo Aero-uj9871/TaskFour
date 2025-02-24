bb5a8676c666301e8140370b8940c71583ea2b3c
Aero-uj9871 <ujjwals@aeroqube.com>
2025-02-24 11:12:03 +0530

    updated NAVBAR

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,35 +1,3 @@
-// import { Link } from "react-router-dom";
-// import logo from "../assets/logo.svg";
-// function Navbar() {
-//   return (
-//     <nav className="bg-[#3c64b1]/6 shadow-sm p-4 flex items-center justify-between lg:justify-around md:gap-10 overflow-x-hidden">
-//       <div className=""><Link to="/"><img src={logo} alt="" className="h-[30px] object-contain w-full" /></Link></div>
-//       <div className=""> 
-//         <button data-collapse-toggle="navbar-sticky" type="button" className="  inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none   dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
-//           <span className="sr-only">Open main menu</span>
-//           <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-//             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
-//           </svg>
-//         </button>
-//       </div>
-//       <ul className="hidden lg:flex text-nowrap justify-around space-x-6 text-md md:space-x-6 items-center  sm:{w-120 mr-4}">
-//         <li><Link to="/" className="text-navTextCol font-navabarLinks hover:text-[#5b70a5]">Home</Link></li>
-//         <li><Link to="/about" className="text-navTextCol font-navabarLinks hover:text-[#5b70a5]">About Us</Link></li>
-//         <li><Link to="/villas" className="text-navTextCol font-navabarLinks hover:text-[#5b70a5]">Villas</Link></li>
-//         <li><Link to="/services" className="text-navTextCol font-navabarLinks hover:text-[#5b70a5]">Services</Link></li>
-//         <li><Link to="/partners" className="text-navTextCol font-navabarLinks hover:text-[#5b70a5]">Partner with us</Link></li>
-//         <li><Link to="/contactUs" className="bg-primary font-navabarLinks text-[14px] text-white px-4 py-2 rounded hover:bg-[#5b70a5]">Contact</Link></li>
-//       </ul>
-//     </nav>
-//   );
-// }
-
-// export default Navbar;
-
-
-
-
-
 import { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import logo from "../assets/logo.svg";
@@ -85,7 +53,7 @@ const Navbar = () => {
 
       {/* Mobile Side Navbar */}
       <div
-        className={`fixed top-0 left-0 w-64 h-full bg-white shadow-lg transform ${isOpen ? "translate-x-0" : "-translate-x-full"
+        className={`fixed top-0 left-0 w-64 h-full hover:text-[#6d87cb] bg-[#ebecef] shadow-lg transform ${isOpen ? "translate-x-0" : "-translate-x-full"
           } transition-transform duration-300 ease-in-out lg:hidden z-50`}
       >
         {/* Close Button */}
@@ -110,7 +78,7 @@ const Navbar = () => {
               key={to}
               to={to}
               onClick={() => setIsOpen(false)}
-              className="tetx-[#373f41] text-lg font-semibold text-gray-700 hover:text-[#6d87cb] transition-colors"
+              className="text-[#373f41] text-lg font-semibold hover:text-[#6d87cb] transition-colors"
             >
               {label}
             </NavLink>
